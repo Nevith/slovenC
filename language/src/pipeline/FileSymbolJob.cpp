bb0f582cd7da0d9402296ecd9ff7d97abdb334c8
Andraž <pika.andraz.kralj@gmail.com>
2020-03-17 11:09:45 +0100

    [Add] Added FileSymbolJob

---
+++ language/src/pipeline/FileSymbolJob.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andraz on 17/03/2020.
+//
+
+#include "FileSymbolJob.h"
---
+++ language/src/pipeline/FileSymbolJob.h
@@ -0,0 +1,16 @@
+//
+// Created by Andraz on 17/03/2020.
+//
+
+#ifndef SLOVENC_FILESYMBOLJOB_H
+#define SLOVENC_FILESYMBOLJOB_H
+
+
+
+class FileSymbolJob {
+
+};
+
+
+
+#endif //SLOVENC_FILESYMBOLJOB_H
